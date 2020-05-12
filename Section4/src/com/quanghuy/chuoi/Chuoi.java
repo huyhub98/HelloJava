89672cccf8193c554037cca1417c90b49a889118
Nguyen Quang Huy <Huynguyenquang98@gmail.com>
2020-05-12 17:55:44 +0700

    section4 testing

---
+++ Section4/src/com/quanghuy/chuoi/Chuoi.java
@@ -0,0 +1,48 @@
+package com.quanghuy.chuoi;
+
+import java.util.Scanner;
+
+public class Chuoi {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập vào chuỗi bất kỳ: ");
+		String string1 = sc.nextLine().toLowerCase();
+		System.out.println("Nhập vào chuỗi bất kỳ tiếp theo");
+		String string2 = sc.nextLine();
+		stringLength(string1);
+		compareChuoi(string1, string2);
+		count(string1);
+	}
+
+	// In ra độ dài của string1
+	public static void stringLength(String string1) {
+		System.out.println("Độ dài của chuỗi là: " + string1.length());
+	}
+
+	// So sánh 2 chuỗi
+	public static void compareChuoi(String string1, String string2) {
+		if (string1.equalsIgnoreCase(string2)) {
+			System.out.println("Hai chuỗi bằng nhau");
+		} else {
+			System.out.println("Hai chuỗi không bằng nhau");
+		}
+	}
+
+	// Đếm số kí tự A xuất hiện trong chuỗi
+	public static void count(String string1) {
+		char kyTu1 = 'a';
+		char kyTu2 = 'ạ';
+		char kyTu3 = 'á';
+		char kyTu4 = 'ã';
+		char kyTu5 = 'ạ';
+		char kyTu6 = 'ả';
+		int count = 0;
+		for (int i = 0; i < string1.length(); i++) {
+			if (string1.charAt(i) == kyTu1 || string1.charAt(i) == kyTu2 || string1.charAt(i) == kyTu3
+					|| string1.charAt(i) == kyTu4 || string1.charAt(i) == kyTu5 || string1.charAt(i) == kyTu6) {
+				count++;
+			}
+		}
+		System.out.println("Chuỗi có " + count + " lần kí tự A xuất hiện trong chuỗi " + string1);
+	}
+}
