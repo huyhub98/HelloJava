8513516e1acd88c3eda1228454e9a688f14f3c15
huyhub98 <huynguyenquang98@gmail.com>
2020-10-29 00:56:13 +0700

    CRUD updated

--- transport/src/main/java/com/ifi/transport/controller/TransportController.java
+++ transport/src/main/java/com/ifi/transport/controller/TransportController.java
@@ -4,7 +4,6 @@ import com.ifi.transport.dao.TransportRepository;
 import com.ifi.transport.dao.impl.TransportDaoImpl;
 import com.ifi.transport.entity.Transport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,10 +19,24 @@ import java.util.List;
 public class TransportController {
     @Autowired
     private TransportRepository transportRepository;
+
     @Autowired
     private TransportDaoImpl transportDao;
     public static List<Transport> transportList = new ArrayList<Transport>();
 
+    // create
+    @GetMapping(value = "/create")
+    public String create() {
+        return "Create";
+    }
+
+    @PostMapping(value = "/create")
+    public String create(@ModelAttribute Transport transport) {
+        transportRepository.save(transport);
+        return "redirect:/display";
+    }
+
+    // display
     @GetMapping(value = "/display")
     public String show(Model model, @RequestParam(value = "keyword", required = false) String keyword) {
         if (keyword == null) {
@@ -34,28 +45,28 @@ public class TransportController {
         transportList = transportRepository.search("%" + keyword + "%");
         model.addAttribute("trans", transportList);
         model.addAttribute("keyword", keyword);
-        return "display";
+        return "Display";
     }
 
-//    @GetMapping(value = "/display")
-//    public String show(Model model,HttpServletRequest req) {
-//        //transportList = transportDao.getList();
-//        transportList = transportRepository.show();
-//        //model.addAttribute("trans", transportList);
-//        req.setAttribute("trans",transportList);
-//        return "display";
-//    }
-
+    // delete
     @GetMapping(value = "/delete")
     public String delete(@RequestParam(value = "id") int id) {
         transportRepository.deleteById(id);
         return "redirect:/display";
     }
 
+    // update
     @GetMapping(value = "/update")
-    public String update(@RequestParam(value = "id") int id, HttpServletRequest req) {
+    public String update(@RequestParam(value = "id") int id, Model model) {
         Transport trans = transportRepository.getOne(id);
-        req.setAttribute("trans", trans);
-        return "update";
+        model.addAttribute("tran", trans);
+        return "Update";
+    }
+
+    @PostMapping(value = "/update")
+    public String update(@ModelAttribute (name = "transport")Transport transport) {
+        transportRepository.saveAndFlush(transport);
+        //transportDao.update(transport);
+        return "redirect:/display";
     }
 }
