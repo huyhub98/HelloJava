d3970a68cc525717835a86a3030ad3589d378509
huyhub98 <huynguyenquang98@gmail.com>
2020-11-13 00:18:33 +0700

    update REST

--- transport/src/main/java/com/ifi/transport/controller/UserRestController.java
+++ transport/src/main/java/com/ifi/transport/controller/UserRestController.java
@@ -4,8 +4,7 @@ import com.ifi.transport.model.UserDTO;
 import com.ifi.transport.repository.UserRepository;
 import com.ifi.transport.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +15,27 @@ public class UserRestController {
     @Autowired
     private UserRepository userRepository;
 
-
     @GetMapping("/list")
-    public List<UserDTO> getList(){
+    public List<UserDTO> getList() {
         return userService.getAll();
+    }
+
+    @PostMapping("/new-user")
+    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        userService.create(userDTO);
+        return userDTO;
+    }
+
+    @PutMapping("/update-user/{id}")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        userService.update(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/delete-user/{id}")
+    public String deleteUser(@PathVariable int id) {
+        //userRepository.deleteById(id);
+        userService.delete(id);
+        return "deleted user with id: " + id;
     }
 }
